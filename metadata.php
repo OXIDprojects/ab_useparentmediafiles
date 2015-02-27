2e3593486d89a322077be58f9859212bd325592b
Alfred Bez <a.bez@bkm-medien.de>
2015-02-27 12:31:03 +0100

    [TASK] version 1.0.1

--- metadata.php
+++ metadata.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Version:    1.0
+ * Version:    1.0.1
  * Author:     Alfred Bez <alfred.bez@gmail.com>
  * Author URI: http://www.alfredbez.de
  */
@@ -18,7 +18,7 @@ $aModule = array(
     'id'           => 'ab_useparentmediafiles',
     'title'        => 'Alfred Bez :: use parent Media Files',
     'description'  => 'Wenn ein Variante eines Artikels keine eigenen Medien-Dateien hat, werden die des Elternartikels geladen',
-    'version'      => '1.0',
+    'version'      => '1.0.1',
     'author'       => 'Alfred Bez',
     'url'          => 'http://www.bkm-medien.de',
     'email'        => 'a.bez@bkm-medien.de',
